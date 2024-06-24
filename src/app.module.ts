8b3595ffa390457eb84128167cda5bf65580f565
tavata <tavata.canales@gmail.com>
2024-06-24 09:25:10 -0500

    docker/mongo añadido conexion basica a mongo ehcha

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
@@ -8,6 +10,8 @@ import { ServeStaticModule } from '@nestjs/serve-static';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    PokemonModule,
   ],
   controllers: [],
   providers: [],
