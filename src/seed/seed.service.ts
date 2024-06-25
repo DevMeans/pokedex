e09a3f5dbf9cbf1b7057dc2a524dfbee12726fe0
tavata <tavata.canales@gmail.com>
2024-06-25 15:28:21 -0500

    seed implementado

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { pokemonResponse } from './interfaces/pokemon-response.interface';
-
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios; //TODO:Crea una dependecia no esta injectado
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
     const { data } = await this.axios.get<pokemonResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0',
     );
-    data.results.forEach(({name,url}) => {
-      const segment =url.split('/');
-      const no = +segment[segment.length-2]
-      console.log({name,no})
+    let datamap = data.results.map(({ name, url }) => {
+      const segment = url.split('/');
+      const no = +segment[segment.length - 2];
+      return {
+        no: no,
+        name: name,
+      };
     });
-    return data.results;
+    const inserts = await this.pokemonModel.insertMany(datamap);
+    if(!inserts){
+      return 'Error inesperado';
+    }
+    return 'Seed ejecutado'
+   
   }
 }
